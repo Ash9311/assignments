9dca3e22d5dadac9ea2d3a68b7511a009a285e29
@Ash9311 <ashutoshmulki7@gmail.com>
2023-12-05 23:59:11 +0530

    anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,9 @@
 */
 
 function isAnagram(str1, str2) {
-  
+  const sortedstr1=str1.split('').sort().join(''); //cannot sort string directly in js. so first convert to array,sort,make it to back
+  const sortedstr2=str2.split('').sort().join('');
+  return sortedstr1==sortedstr2;
 }
 
 module.exports = isAnagram;
